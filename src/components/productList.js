240ad036d6fa6bca4ee8ebad08e246360e504678
jnauq <justin.aq88@gmail.com>
2020-08-14 11:56:53 -0700

    added error message for http

--- src/components/productList.js
+++ src/components/productList.js
@@ -7,28 +7,33 @@ class ProductList extends Component {
     constructor() {
         super()
         this.state = {
-            products : []
+            products : [],
+            errorMsg : ''
         }
     }
 
     componentDidMount() {
         axios.get('https://amazon-api-082020.herokuapp.com/api/products')
             .then(response => {
-                this.setState({products: response.data})
+                this.setState({ products: response.data })
             })
             .catch(error => {
-                console.log(error)
+                this.setState({ errorMsg: 'Error retrieving data' })
             })
     }
   
     render() {
+        const { products, errorMsg } = this.state
         return (
             <div>
                 <div className="productwrapper" style={listWrapperStyle}>
                     <ul className="listwrapper" style={listStyle}>
-                    {this.state.products.map((element) => (
-                        <Product key={element.id} data={element}/>
-                    ))}
+                    {
+                        products.length ?
+                        products.map((element) => (<Product key={element.id} data={element}/>)) : null
+                    }
+
+                    { errorMsg ? <div>{errorMsg}</div> : null }
                     </ul>
                 </div>
             </div>
