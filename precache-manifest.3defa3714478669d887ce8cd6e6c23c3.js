026d0de7c9e681624efce5e04f3e09a9ad08f82f
jnauq <justin.aq88@gmail.com>
2020-08-20 14:04:38 -0700

    Updates

---
+++ precache-manifest.3defa3714478669d887ce8cd6e6c23c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62aeb35d2fbc90b6c94d8bfbf3ac9fa",
+    "url": "/pricetracker-frontend/index.html"
+  },
+  {
+    "revision": "131bce7815e092f9b71d",
+    "url": "/pricetracker-frontend/static/css/main.40b5b214.chunk.css"
+  },
+  {
+    "revision": "a1152a6e8f93d9b25bb2",
+    "url": "/pricetracker-frontend/static/js/2.44c99a6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pricetracker-frontend/static/js/2.44c99a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131bce7815e092f9b71d",
+    "url": "/pricetracker-frontend/static/js/main.b5653b9b.chunk.js"
+  },
+  {
+    "revision": "d4215ab0fcd9f2b3f379",
+    "url": "/pricetracker-frontend/static/js/runtime-main.ea135fdf.js"
+  }
+]);
